bb8a59dbaa817106ec4023ecf97b030f1895f2e0
Ashish Kumar <xashishkumar@outlook.com>
2026-04-26 17:21:14 +0530

    authentication system setup

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profile/<my_id>', views.profile, name='profile'),
+    path('profile/<int:my_id>', views.profile, name='profile'),
+    path('register', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('forgot/', views.forgot, name='forgot'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
 def profile(request, my_id):
+
+    messages.success(request, "successfully fetch")
     return render(request, 'profile.html', {'id': my_id})
+
+
+
+def register(request):
+
+    return render(request, 'register.html')
+
+def login(request):
+
+    return render(request, 'login.html')
+
+def forgot(request):
+
+    return render(request, 'forgot.html')
